cd0c21cb965d4176e514441c15fbca20e442089b
unknown <yhdosa0622@gmail.com>
2024-03-27 08:27:28 +0900

    001. 자바프로그래밍 기초

---
+++ example/src/Test.java
@@ -0,0 +1,15 @@
+
+public class Test {
+	
+	public static void main (String[] args) {
+		System.out.println("aaa");
+	}
+}
+
+class One {
+	
+}
+
+class Two {
+	
+}
---
+++ example/src/example/Ex01.java
@@ -0,0 +1,10 @@
+package example;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		System.out.println("수업 끝 신남");
+
+	}
+
+}
