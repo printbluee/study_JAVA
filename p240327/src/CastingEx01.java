93371a3a9d798cd5c331f409e61c7697c573e585
unknown <yhdosa0622@gmail.com>
2024-03-27 18:25:13 +0900

    001. 자바프로그래밍 기초 - 이클립스 에러, 문자와 문자열, 리터럴, 연산자 종류, 퀴즈, 형 변환

---
+++ p240327/src/CastingEx01.java
@@ -0,0 +1,19 @@
+
+public class CastingEx01 {
+
+	public static void main(String[] args) {
+		
+		// [ 형변환 ] --> 타입 변환
+		// 강제 형변환, 명시적 형변환	
+		// 자동 형변환, 묵시적 형변환
+		
+		int n1 = 9;
+		int n2 = 2;
+		System.out.println("n1/n2 => " + n1/n2);
+		System.out.println("n1/(float)n2 => " + n1/(float)n2);		
+		System.out.println("(float)n1/n2 => " + (float)n1/n2);
+		System.out.println("(float)(n1/n2) => " + (float)(n1/n2));
+		
+	}
+
+}
