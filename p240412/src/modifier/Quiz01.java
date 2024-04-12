8bac2c4f9f331e5b95ae51f102888f552feb6e5b
unknown <yhdosa0622@gmail.com>
2024-04-12 18:44:52 +0900

    240412_과제

--- p240412/src/modifier/Quiz01.java
+++ p240412/src/modifier/Quiz01.java
@@ -20,6 +20,13 @@ class Banana extends Fruit {
 	// [ 문제 2 ] 생성자를 이용하여 부모 class Fruit 의 count field 에 값 설정
 	
 	// [ 문제 1 ] showInfo() 메서드를 오버라이딩(overriding) 하시오.
+	Banana () {
+		System.out.println("현재 과일은 바나나입니다.");
+	}
+	
+	public void showInfo(int count) {
+		System.out.println("바나나는 " + count + "개 있습니다.");
+	}
 }
 
 
@@ -28,13 +35,13 @@ public class Quiz01 {
 		
 		// [ 문제 1 ]
 		Banana bananaOne = new Banana();
-		bananaOne.showInfo();
+		bananaOne.showInfo(0);
 		// 현재 과일은 바나나입니다.
 		// 바나나는 0개 있습니다.
 		
 		// [ 문제 2 ]
 		Banana bananaTwo = new Banana();
-		bananaTwo.showInfo();
+		bananaTwo.showInfo(10);
 		// 현재 과일은 바나나입니다.
 		// 바나나는 10개 있습니다.
 		
